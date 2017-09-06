ff7a37f729f13bb79943e20b837c388c6d7b7ae2
Luc Merceron <luc.merceron@wing.eu>
2017-09-06 09:45:08 +0200

    [+] GroceryItem component implementation

--- src/components/presentationals/groceries/GroceryItem.js
+++ src/components/presentationals/groceries/GroceryItem.js
@@ -4,7 +4,31 @@ import PropTypes from 'prop-types'
 import './GroceryItem.css'
 
 const GroceryItem = ({ item, quantity, addItem, removeItem }) => (
-  <div className="grocery-shop-item" />
+  <div className="grocery-shop-item">
+    <div className="grocery-shop-item-left">
+      <div className="grocery-shop-item-picture" style={{ backgroundImage: `url('${item.picture}')` }} />
+      <div className="grocery-shop-item-price">
+        {item.price}
+        <span className="grocery-shop-item-cts">CTS</span>
+      </div>
+      <div className="grocery-shop-item-actions">
+        <div className="grocery-shop-item-add" onClick={addItem} />
+        <div className="grocery-shop-item-remove" onClick={removeItem} />
+      </div>
+    </div>
+    <div className="grocery-shop-item-right">
+      <div className="grocery-shop-item-name">{item.name}</div>
+      <div className="grocery-shop-item-stats">
+        {Object.keys(item.stats).map(key => (
+          <div key={key} className="grocery-shop-item-stat">
+            <span className="grocery-shop-item-stat-desc">{key}</span>
+            <span className="grocery-shop-item-stat-val">{item.stats[key]}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+    <div className="grocery-shop-item-quantity">{quantity}</div>
+  </div>
 )
 
 GroceryItem.propTypes = {
