0f1b301952e7fa87e01c6690c6b6ab530af36a19
Luc Merceron <luc.merceron@wing.eu>
2017-09-06 21:28:49 +0200

    [+] TopBar styles [*] TopBar refactoring

--- src/components/presentationals/topBar/TopBar.js
+++ src/components/presentationals/topBar/TopBar.js
@@ -5,11 +5,15 @@ import './TopBar.css'
 
 const TopBar = ({ logo, name }) => (
   <div className="grocery-shop-top-bar">
-    <div className="grocery-shop-top-bar-logo" style={{ backgroundImage: `url('${logo}')` }} />
+    <div className="grocery-shop-top-bar-left">
+      <div className="grocery-shop-top-bar-logo" style={{ backgroundImage: `url('${logo}')` }} />
+    </div>
     <div className="grocery-shop-top-bar-title">
-      <p className="grocery-shop-top-bar-title-welcome">Welcome</p>
-      <p className="grocery-shop-top-bar-title-name">{name}</p>
-      <p className="grocery-shop-top-bar-title-info">Your grocery list</p>
+      <div className="grocery-shop-top-bar-title-welcome">
+        Welcome
+        <div className="grocery-shop-top-bar-title-name">{name}</div>
+      </div>
+      <div className="grocery-shop-top-bar-title-info">Your grocery list</div>
     </div>
   </div>
 )
