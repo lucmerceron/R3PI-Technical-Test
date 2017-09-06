b756909d1fb3751bb69dd81c03e7cae0767ad369
Luc Merceron <luc.merceron@wing.eu>
2017-09-06 21:29:58 +0200

    [+] BottomContainer styles + refactoring

--- src/components/presentationals/bottomBar/BottomContainer.js
+++ src/components/presentationals/bottomBar/BottomContainer.js
@@ -12,30 +12,47 @@ class BottomContainer extends React.Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    const { cart } = this.props
+
+    // The children should not be visible if the cart became empty
+    if (nextProps.cart.length === 0 && cart.length > 0) {
+      this.setState({ visible: false })
+    }
+  }
+
   render() {
     const { cart, children } = this.props
     const { visible } = this.state
 
     if (cart.length === 0) return null
-
     return (
       <div
         className={`grocery-shop-bottom-bar${visible ? ' visible' : ''}`}
         onClick={() => this.setState({ visible: true })}
       >
-        <div className="grocery-shop-bottom-bar-item-count">{cart.length}</div>
-        <div
-          className="grocery-shop-bottom-bar-close"
-          onClick={(event) => {
-            event.stopPropagation()
-            this.setState({ visible: false })
-          }}
-        />
-        <div className="grocery-shop-bottom-bar-children">
-          {children}
+        <div className="grocery-shop-bottom-bar-visible-off">
+          <div className="grocery-shop-bottom-bar-item-count"><span>{cart.length}</span></div>
+          <div className="grocery-shop-bottom-bar-info">Open your cart</div>
+        </div>
+        <div className="grocery-shop-bottom-bar-visible-on">
+          <div className="grocery-shop-bottom-bar-top">
+            <span>grocery</span>
+            <div
+              className="grocery-shop-bottom-bar-close material-icons"
+              onClick={(event) => {
+                event.stopPropagation()
+                this.setState({ visible: false })
+              }}
+            >
+              &#xE5CD;
+            </div>
+          </div>
+          <div className="grocery-shop-bottom-bar-children">
+            {children}
+          </div>
         </div>
       </div>
-
     )
   }
 }
