3409340cb669834871d6c3c528942c961ba519d1
Luc Merceron <luc.merceron@wing.eu>
2017-09-06 08:58:56 +0200

    [*] App provider & container call

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
-import './App.css'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Provider } from 'react-redux'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
+import GroceryShop from './components/containers/GroceryShop'
+
+const App = ({ store }) => (
+  <Provider store={store}>
+    <GroceryShop />
+  </Provider>
+)
+
+App.propTypes = {
+  store: PropTypes.object.isRequired,
 }
 
 export default App
+
