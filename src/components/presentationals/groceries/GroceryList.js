66c98725654e2b0e851864660694984bdb468045
Luc Merceron <luc.merceron@wing.eu>
2017-09-06 10:18:31 +0200

    [+] GroceryList implementation

--- src/components/presentationals/groceries/GroceryList.js
+++ src/components/presentationals/groceries/GroceryList.js
@@ -7,6 +7,18 @@ import './GroceryList.css'
 
 const GroceryList = ({ items, cart, addItem, removeItem }) => (
   <div className="grocery-shop-items">
+    {items.map(item => (
+      <GroceryItem
+        key={item.id}
+        item={item}
+        quantity={cart.reduce((acc, itemId) => {
+          if (itemId === item.id) return acc + 1
+          return acc
+        }, 0)}
+        addItem={addItem}
+        removeItem={removeItem}
+      />
+    ))}
   </div>
 )
 
