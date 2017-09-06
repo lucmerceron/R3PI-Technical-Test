6799592765c3f988eaa8788c02b27f10bc513697
Luc Merceron <luc.merceron@wing.eu>
2017-09-06 11:29:33 +0200

    [+] BottomContainer component implementation

--- src/components/presentationals/bottomBar/BottomContainer.js
+++ src/components/presentationals/bottomBar/BottomContainer.js
@@ -3,9 +3,42 @@ import PropTypes from 'prop-types'
 
 import './BottomContainer.css'
 
-const BottomContainer = ({ cart, children }) => (
-  <div className="grocery-shop-bottom-bar" />
-)
+class BottomContainer extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      visible: false,
+    }
+  }
+
+  render() {
+    const { cart, children } = this.props
+    const { visible } = this.state
+
+    if (cart.length === 0) return null
+
+    return (
+      <div
+        className={`grocery-shop-bottom-bar${visible ? ' visible' : ''}`}
+        onClick={() => this.setState({ visible: true })}
+      >
+        <div className="grocery-shop-bottom-bar-item-count">{cart.length}</div>
+        <div
+          className="grocery-shop-bottom-bar-close"
+          onClick={(event) => {
+            event.stopPropagation()
+            this.setState({ visible: false })
+          }}
+        />
+        <div className="grocery-shop-bottom-bar-children">
+          {children}
+        </div>
+      </div>
+
+    )
+  }
+}
 
 BottomContainer.propTypes = {
   cart: PropTypes.array.isRequired,
